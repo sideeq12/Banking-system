88a78eaccf19272803b5c14ca133aae27449a4b5
sideeq12 <abdwaheed2018@gmail.com>
2021-06-23 19:47:56 +0100

    mode skeleton with css

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 // Declaring the modules to be used in the app
 const express = require("express")
-
+const bodyParser = require("body-parser")
+const path = require("path")
 
 const app = express()
 
+app.set("view engine", "ejs")
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, "public")))
+// app.set("views", path.join(__dirname, "views"))
+// app.set("views", "./views")
+
 
 app.get("/", (req, res)=>{
-    res.send("Hello world")
+    res.render("welcome")
 })
 
 
