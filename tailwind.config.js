f4980f7fdd6b1bc85e77de40bfc4d1bcbb00f747
sideeq12 <abdwaheed2018@gmail.com>
2021-06-25 19:22:35 +0100

    new change

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage : theme =>({
+        'hero-pattern' : "url('/public/images/image1.svg')"
+      })
+    },
   },
   variants: {
     extend: {},
