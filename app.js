90b0c08782ed40d9957b074c0725693d3f0ca97d
sideeq12 <abdwaheed2018@gmail.com>
2021-06-26 19:07:24 +0100

    hope

--- app.js
+++ app.js
@@ -8,13 +8,37 @@ const app = express()
 app.set("view engine", "ejs")
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, "public")))
-// app.set("views", path.join(__dirname, "views"))
-// app.set("views", "./views")
 
 
+// All get request of the projects
 app.get("/", (req, res)=>{
     res.render("welcome")
 })
+
+app.get("/create", (req, res)=>{
+    res.render("sign-up")
+})
+
+app.get("/login", (req, res)=>{
+    res.render("signin")
+})
+
+app.get("/transfer", (req, res)=>{
+    res.render("send")
+})
+
+app.get("/withdraw", (req, res)=>{
+    res.render("withdraw")
+})
+
+// THE POST REQUESTS FOR HANDLING DATA
+app.post("/dashboard", (req, res)=>{
+    res.render("dashboard")
+})
+app.post("/success", (req, res)=>{
+    res.render("success")
+})
+
 
 
 const port = process.env.PORT || 3030
